b4e25f95446729e7d16f3e19697703163fff8d37
akiraseto <akira.seto@gmail.com>
2021-03-17 23:36:26 +0900

    GPIO:BMP180センサー値取得、mongoにインサートを実装

--- sensors/app.py
+++ sensors/app.py
@@ -9,15 +9,18 @@ import os
 import time
 
 from models.daikin import Daikin
+from models.gpio import Gpio
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
 
     daikin = Daikin()
+    gpio = Gpio()
 
     minutes_time = int(os.environ['BASIC_MINUTE'])
 
     latest_record = {}
+    latest_record_gpio = {}
     periods = ['minute', 'hour', 'day', 'week']
     delta_list = {}
     delta_list['minute'] = datetime.timedelta(minutes=1)
@@ -26,21 +29,34 @@ if __name__ == '__main__':
     delta_list['week'] = datetime.timedelta(weeks=1)
 
     # 初期化:DBにデータ無ければ即インサート
-    data = daikin.get_sensor()
+    data_daikin = daikin.get_sensor()
+    data_gpio = gpio.get_sensor()
     for period in periods:
         latest_record[period] = daikin.find_latest(period)
         if latest_record[period] is None:
-            daikin.insert_data(period, data)
-            latest_record[period] = data
+            daikin.insert_data(period, data_daikin)
+            latest_record[period] = data_daikin
+
+        latest_record_gpio[period] = gpio.find_latest(period)
+        if latest_record_gpio[period] is None:
+            gpio.insert_data(period, data_gpio)
+            latest_record[period] = data_gpio
 
     while True:
         now = datetime.datetime.now()
-        data = daikin.get_sensor()
-        logging.info('daikin:{}'.format(data))
+        data_daikin = daikin.get_sensor()
+        data_gpio = gpio.get_sensor()
+        logging.info('daikin:{}'.format(data_daikin))
+        logging.info('gpio:{}'.format(data_gpio))
 
         for period in periods:
             if now >= latest_record[period]['timestamp'] + delta_list[period]:
-                daikin.insert_data(period, data)
-                latest_record[period] = data
+                daikin.insert_data(period, data_daikin)
+                latest_record[period] = data_daikin
+
+            if now >= latest_record_gpio[period]['timestamp'] \
+                    + delta_list[period]:
+                gpio.insert_data(period, data_gpio)
+                latest_record_gpio[period] = data_gpio
 
         time.sleep(minutes_time * 60)
---
+++ sensors/models/gpio.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""ダイキン."""
+
+from datetime import datetime
+
+import Adafruit_BMP.BMP085 as BMP085
+
+from db import MongoDB
+
+
+class Gpio(MongoDB):
+    """Daikin IO."""
+
+    def __init__(self):
+        """イニシャライザ."""
+        super().__init__()
+
+        # センサー
+        self.sensor = BMP085.BMP085()
+
+        # MongoDB
+        db_name = 'gpio'
+        self.db = self.client[db_name]
+
+    def get_sensor(self):
+        """Get センサー値.
+
+        Returns
+        -------
+        dict
+            各センサー値
+
+        """
+        return {
+            'temp': self.sensor.read_temperature(),
+            'press': self.sensor.read_pressure(),
+            'alti': self.sensor.read_altitude(),
+            'sealev': self.sensor.read_sealevel_pressure(),
+            'timestamp': datetime.now()
+        }
+
+    def insert_data(self, periods, data):
+        """
+        センサーデータを保存.
+
+        Parameters
+        ----------
+        periods : str
+            'minute','hour', 'day', 'week'
+        data : dict
+            センサーデータ
+
+        Returns
+        -------
+        None
+
+        """
+        db_col = self.db.get_collection(periods)
+        db_col.insert_one(data)
+
+    def find_latest(self, periods):
+        """
+        DBから最新データを1つ取得.
+
+        Parameters
+        ----------
+        periods : str
+            'minute','hour', 'day', 'week'
+        data : dict
+            センサーデータ
+
+        Returns
+        -------
+        dict
+            最新データ1つ
+
+        """
+        db_col = self.db.get_collection(periods)
+        res = db_col.find_one(sort=[('timestamp', -1)],
+                              projection={'_id': 0, })
+        return res
